7e64300ec4dc664695353200fd4cc3d79846645d
Adiks <apacholak97@interia.pl>
2023-02-21 14:57:01 +0100

    error handling

--- src/test/java/pl/adrian/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
+++ src/test/java/pl/adrian/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -12,6 +12,25 @@ class PersonRepositoryImplTest {
     PersonRepository personRepository = new PersonRepositoryImpl();
 
     @Test
+    void testFindPersonByIdNotFound() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        final Integer id = 8;
+
+        Mono<Person> personMono = personFlux.filter(person -> person.getId() == id).single()
+                .doOnError(throwable -> {
+                    System.out.println("Error occurred in Flux");
+                    System.out.println(throwable);
+                });
+
+        personMono.subscribe(System.out::println,
+                throwable -> {
+                    System.out.println("Error occurred in Mono");
+                    System.out.println(throwable);
+                });
+    }
+
+    @Test
     void testGetById() {
         Mono<Person> fionaMono = personRepository.findAll()
                 .filter(person -> person.getFirstName().equals("Fiona"))
