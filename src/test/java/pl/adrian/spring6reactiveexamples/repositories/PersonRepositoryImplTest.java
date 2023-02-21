b0343fb9aad601964abce141c91b1b5385858ae4
Adiks <apacholak97@interia.pl>
2023-02-21 14:34:33 +0100

    flux operations

--- src/test/java/pl/adrian/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
+++ src/test/java/pl/adrian/spring6reactiveexamples/repositories/PersonRepositoryImplTest.java
@@ -2,13 +2,53 @@ package pl.adrian.spring6reactiveexamples.repositories;
 
 import org.junit.jupiter.api.Test;
 import pl.adrian.spring6reactiveexamples.domain.Person;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonRepositoryImplTest {
 
     PersonRepository personRepository = new PersonRepositoryImpl();
+
+    @Test
+    void testFluxToList() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Mono<List<Person>> personListMono = personFlux.collectList();
+
+        personListMono.subscribe(personList -> {
+            personList.forEach(person -> {
+                System.out.println(person.getFirstName());
+            });
+        });
+    }
+
+    @Test
+    void testFluxMap() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.map(Person::getFirstName)
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxSubscribers() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Person person = personFlux.blockFirst();
+
+        System.out.println(person);
+    }
 
     @Test
     void testMapOperation() {
